152b6c774fc19b31c9d276f4b51aa7b55fceaec1
SKREFI <srazvan43@yahoo.com>
2018-05-28 12:16:54 +0300

    Initial commit

---
+++ app/src/main/java/com/skrefi/bookstore/data/BookContract.java
@@ -0,0 +1,20 @@
+package com.skrefi.bookstore.data;
+
+import android.provider.BaseColumns;
+
+public final class BookContract {
+
+    private BookContract(){}
+
+    public static final class BookEntry implements BaseColumns{
+        public final static String TABLE_NAME = "books";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_BOOK_NAME = "name";
+        public final static String COLUMN_BOOK_PRICE = "price";
+        public final static String COLUMN_BOOK_QUANTITY = "quantity";
+        public final static String COLUMN_BOOK_SUPPLIER = "supplier";
+        public final static String COLUMN_BOOK_PHONE = "phone";
+    }
+
+}
---
+++ app/src/main/java/com/skrefi/bookstore/data/BookDbHelper.java
@@ -0,0 +1,43 @@
+package com.skrefi.bookstore.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import static com.skrefi.bookstore.data.BookContract.BookEntry.COLUMN_BOOK_NAME;
+import static com.skrefi.bookstore.data.BookContract.BookEntry.COLUMN_BOOK_PHONE;
+import static com.skrefi.bookstore.data.BookContract.BookEntry.COLUMN_BOOK_PRICE;
+import static com.skrefi.bookstore.data.BookContract.BookEntry.COLUMN_BOOK_QUANTITY;
+import static com.skrefi.bookstore.data.BookContract.BookEntry.COLUMN_BOOK_SUPPLIER;
+import static com.skrefi.bookstore.data.BookContract.BookEntry.TABLE_NAME;
+import static com.skrefi.bookstore.data.BookContract.BookEntry._ID;
+
+public class BookDbHelper extends SQLiteDatabase {
+
+    public static final String LOG_TAG = BookDbHelper.class.getSimpleName();
+    private static final String DATABASE_NAME = "books.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public BookDbHelper(Context c){
+        super(c,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String SQL_CREATE_BOOKS_TABLE = "CREATE TABLE " +
+                TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_BOOK_NAME + " TEXT NOT NULL, " +
+                COLUMN_BOOK_PRICE + " INTEGER NOT NULL, " +
+                COLUMN_BOOK_QUANTITY + " INTEGER NOT NULL DEFAULT 0, " +
+                COLUMN_BOOK_SUPPLIER + " TEXT DEFAULT \"N/A\", " +
+                COLUMN_BOOK_PHONE + " TEXT DEFAULT \"N/A\");";
+
+        db.execSQL(SQL_CREATE_BOOKS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
+
+}
